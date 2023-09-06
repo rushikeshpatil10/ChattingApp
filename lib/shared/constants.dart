463c030658078bed10e8410e2e0fdd1f113eb82f
rushikeshpatil10 <rushipatil135@gmail.com>
2023-09-06 15:45:47 +0530

    Updated the files

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:669391209154:web:d34de5a49e71e769aff15b";
+  static String apiKey = "AIzaSyCiVG_yCyWI7xEFB7rL2SBlVmWij8YLgds";
+  static String messagingSenderId = "669391209154";
+  static String projectId = "flash-chat-54954";
+  final primaryColor = const Color(0xFFee7b64);
+}
